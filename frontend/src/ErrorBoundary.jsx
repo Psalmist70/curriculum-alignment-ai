a3914d6155dd0321293d28920432e23b8f40d2a7
Psalmist70 <93396284+Psalmist70@users.noreply.github.com>
2026-06-20 16:17:10 +0100

    Create ErrorBoundary.jsx

---
+++ frontend/src/ErrorBoundary.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    console.log("UI ERROR CAUGHT:", error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ padding: "20px", color: "red", textAlign: "center" }}>
+          Something went wrong in the UI. Check console for details.
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
